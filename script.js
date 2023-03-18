a08a28046c717f5c361ddea03909b50a2c0bd24a
aditi291soni <aditi291soni@gmail.com>
2023-03-19 01:53:59 +0530

    X 0 priniting

---
+++ script.js
@@ -0,0 +1,50 @@
+let music = new Audio("music.mp3");
+let ting = new Audio("ting.mp3");
+let gameover = new Audio("gammeover.mp3");
+let turn = "X";
+let gameoverturn = false;
+const TurnChanges = () => {
+  return turn === "X" ? "0" : "X";
+};
+const checkWin = () => {
+  let boxText = document.querySelector(".boxText");
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  wins.forEach((e) => {
+   //  if (
+   //    boxText[e[0]].innerText === boxText[e[1]].innerText &&
+   //    boxText[e[1]].innerText === boxText[e[2]].innerText &&
+   //    boxText[e[0]].innerText !== " "
+   //  ) {
+   //    document.querySelector(".won").innerText ===
+   //      boxText[e[0]].innerText + "WON";
+   //      gameoverturn=true
+   //  }
+  });
+};
+let box = document.getElementsByClassName("box");
+Array.from(box).forEach((elem) => {
+  let boxText = elem.querySelector(".boxText");
+  elem.addEventListener("click", (e) => {
+    if (boxText.innerText === "") {
+      boxText.innerText = turn;
+      console.log("hello");
+      turn = TurnChanges();
+
+      ting.play();
+      checkWin();
+      if(!gameoverturn){
+         document.getElementsByClassName("turns").innerText = " Turn For " + turn;
+
+      }
+    }
+  });
+});
